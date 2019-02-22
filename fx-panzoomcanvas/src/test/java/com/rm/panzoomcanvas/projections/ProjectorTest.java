71f438c8a16933677711dd2128203fdbd0c54db0
Ricardo Marquez <43529704+rmarquez123@users.noreply.github.com>
2019-02-22 05:02:30 -0700

    minor updates.

--- fx-panzoomcanvas/src/test/java/com/rm/panzoomcanvas/projections/ProjectorTest.java
+++ fx-panzoomcanvas/src/test/java/com/rm/panzoomcanvas/projections/ProjectorTest.java
@@ -1,13 +1,14 @@
 package com.rm.panzoomcanvas.projections;
 
-import com.rm.panzoomcanvas.core.Level;
-import com.rm.panzoomcanvas.core.ScreenEnvelope;
-import com.rm.panzoomcanvas.core.VirtualPoint;
-import com.rm.panzoomcanvas.core.VirtualEnvelope;
-import com.rm.panzoomcanvas.core.ScreenPoint;
 import com.rm.panzoomcanvas.core.FxPoint;
+import com.rm.panzoomcanvas.core.GeometryProjection;
+import com.rm.panzoomcanvas.core.Level;
+import com.rm.panzoomcanvas.core.Point;
+import com.rm.panzoomcanvas.core.ScreenEnvelope;
+import com.rm.panzoomcanvas.core.ScreenPoint;
 import com.rm.panzoomcanvas.core.SpatialRef;
-
+import com.rm.panzoomcanvas.core.VirtualEnvelope;
+import com.rm.panzoomcanvas.core.VirtualPoint;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
@@ -20,24 +21,42 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JUnitParamsRunner.class)
 public class ProjectorTest {
-  
+
   public ProjectorTest() {
   }
-  
+
   @Parameters({"0.5, 0.5"})
   @Test
   public void testProjectScreenToVirtualStrict(
-          double screenCnterX, double screenCenterY) {
+    double screenCnterX, double screenCenterY) {
     System.out.println("projectScreenToVirtualStrict");
     ScreenPoint screenMin = new ScreenPoint(0, 0);
     ScreenPoint screenMax = new ScreenPoint(120, 120);
-    
+
     Level level = new Level(0, null);
     ScreenPoint center = new ScreenPoint(60, 60);
     ScreenEnvelope screenEnvVal = new ScreenEnvelope(screenMin, screenMax, level, center);
-    Projector instance = new Projector(new MapCanvasSR(), (FxPoint geomPoint, SpatialRef baseSpatialRef) -> geomPoint);
+//    Projector instance = new Projector(new MapCanvasSR(), (FxPoint geomPoint, SpatialRef baseSpatialRef) -> geomPoint);
+    Projector instance = new Projector(new MapCanvasSR(), new GeometryProjection() {
+      MapCanvasSR sr = new MapCanvasSR(); 
+      
+      @Override
+      public FxPoint project(FxPoint geomPoint, SpatialRef baseSpatialRef) {
+        return geomPoint;
+      }
+
+      @Override
+      public Point getMax() {
+        return sr.getMax();
+      }
+
+      @Override
+      public Point getMin() {
+        return sr.getMin();
+      }
+    });
     VirtualEnvelope result = instance.projectScreenToVirtual(screenEnvVal);
-    
+
     VirtualEnvelope expResult = new VirtualEnvelope(new VirtualPoint(-60, -60), new VirtualPoint(60, 60));
     System.out.println(expResult);
     System.out.println(result);
@@ -47,4 +66,5 @@ public class ProjectorTest {
     Assert.assertEquals("min x equals", expResult.getMin().getX(), result.getMin().getX(), tol);
     Assert.assertEquals("min y equals", expResult.getMin().getX(), result.getMin().getY(), tol);
   }
+
 }
