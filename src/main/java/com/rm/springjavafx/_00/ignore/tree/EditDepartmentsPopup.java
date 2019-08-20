bcebeca52079c28d50922c1f7f1cb78617e708b3
Ricardo Marquez <43529704+rmarquez123@users.noreply.github.com>
2019-08-19 20:48:43 -0600

    testing context menus.

--- src/main/java/com/rm/springjavafx/_00/ignore/tree/EditDepartmentsPopup.java
+++ src/main/java/com/rm/springjavafx/_00/ignore/tree/EditDepartmentsPopup.java
@@ -17,24 +17,27 @@ import org.springframework.stereotype.Component;
 @FxController(fxml = "fxml/theoffice.editdepartment.fxml")
 @PopupComponent(id = "office.editdepartments.popup")
 public class EditDepartmentsPopup implements PopupContent {
-  
+
   private Popup popup;
-  
+
   /**
-   * 
-   * @param popup 
+   *
+   * @param popup
    */
   @Override
   public void setPopupWindow(Popup popup) {
     this.popup = popup;
-    this.popup.showProperty().addListener((obs, old, change)->{
-      this.popup.getUserData(); 
+    this.popup.showProperty().addListener((obs, old, change) -> {
+      if (change) {
+        Object userData = this.popup.getUserData();
+        System.out.println("userdata = " + userData);
+      }
     });
-    
+
   }
 
   @Override
   public void onClose() throws IOException {
   }
-  
+
 }
